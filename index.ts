0c2cbc2258bd4adf6eb55a7573b03a9e24a5e140
Chris Villa <chrisvilla@me.com>
2019-11-25 17:01:09 -0800

    feat: implement basic functionality using image-size

--- index.ts
+++ index.ts
@@ -1,3 +1,25 @@
-export default async (name = 'World'): Promise<string> => {
-  return `Hello ${name}!`
+import { imageSize } from 'image-size'
+import { ISizeCalculationResult } from 'image-size/dist/types/interface'
+import request from 'request-promise-native'
+
+export default async (url: string): Promise<ISizeCalculationResult> => {
+  let body
+
+  try {
+    const response = await request({
+      url,
+      encoding: null,
+      resolveWithFullResponse: true
+    })
+
+    body = response.body
+
+    if (response.headers['content-type'].indexOf('image') === -1) {
+      throw 'URL must return image'
+    }
+  } catch (error) {
+    if (error) throw new Error(error)
+  }
+
+  return imageSize(body)
 }
